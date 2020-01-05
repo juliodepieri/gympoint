70c0de16dbccab176a8a4df87e7fc26b34aadbb4
Júlio de Pieri <juliodepieri@gmail.com>
2020-01-05 20:30:10 -0300

    fix register overlap on checkins list

--- mobile/src/pages/Dashboard/styles.js
+++ mobile/src/pages/Dashboard/styles.js
@@ -3,14 +3,17 @@ import styled from 'styled-components/native';
 export const Container = styled.SafeAreaView`
   background: #f2f2f2;
   height: 100%;
+  flex: 1;
 `;
 
 export const Content = styled.View`
   padding: 20px 20px;
+  flex: 1;
 `;
 
 export const List = styled.FlatList.attrs({
   showsVerticalScrollIndicator: false,
 })`
   padding: 20px 0;
+  flex: 1;
 `;
