757963719b6caae7f95653117b7131312d23249b
Júlio de Pieri <juliodepieri@gmail.com>
2020-01-05 01:29:52 -0300

    fix background on some screens

--- mobile/src/pages/HelpOrder/NewHelpOrder/styles.js
+++ mobile/src/pages/HelpOrder/NewHelpOrder/styles.js
@@ -4,6 +4,7 @@ import Button from '~/components/Button';
 
 export const Container = styled.SafeAreaView`
   background: #f2f2f2;
+  height: 100%;
 `;
 
 export const Form = styled.ScrollView.attrs({
--- mobile/src/pages/HelpOrder/ShowHelpOrder/styles.js
+++ mobile/src/pages/HelpOrder/ShowHelpOrder/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.SafeAreaView`
   background: #f2f2f2;
+  height: 100%;
 `;
 
 export const Content = styled.View`
